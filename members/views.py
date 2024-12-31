f424d74ef596088404ef8469cff88e61166169f1
JB <ecrireajb@gmail.com>
2024-12-31 14:21:06 +0100

    Testing queryset manipulation in Django
    - Modify members/views.py to test queryset manipulation on testing route
    - Update members/templates/template.html for testing purposes

--- members/views.py
+++ members/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Member
+from django.db.models import Q
 
 # Create your views here.
 
@@ -21,7 +22,24 @@ def details(request, member_id):
     return render(request, "details.html", context)
 
 def testing(request):
+    mymembers = Member.objects.all()
+    values = Member.objects.all().values()
+    firstnames = Member.objects.all().values_list('firstname')
+    firstnamesFlat = Member.objects.all().values_list('firstname', flat=True)
+    bobOrLinda = Member.objects.filter(Q(firstname='Bob') | Q(firstname="Linda")).values()
+    startsWithL = Member.objects.filter(firstname__startswith='L').values()
+    sortedMembers = Member.objects.order_by('firstname').values()
+    sortedMembersDesc = Member.objects.order_by('-firstname').values()
+    multisort = Member.objects.order_by('lastname', '-id').values()
     context = {
-        'fruits': ['apple', 'banana', 'cherry', 'date', 'elderberry']
+        'mymembers': mymembers,
+        'values': values,
+        'firstnames': firstnames,
+        'firstnamesFlat': firstnamesFlat,
+        'bobOrLinda': bobOrLinda,
+        'startsWithL': startsWithL,
+        'sortedMembers': sortedMembers,
+        'sortedMembersDesc': sortedMembersDesc,
+        'multisort': multisort
     }
     return render(request, "template.html", context)
